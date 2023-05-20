b6a49e9fb97c11d1c05ff92b05faa27741a6ced3
HexExecute <hex.execute@gmail.com>
2023-05-19 21:09:37 -0400

    Created DHT20 Struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use bsp::hal::prelude::_atsamd_hal_embedded_hal_digital_v2_OutputPin;
+use bsp::pin_alias;
 #[cfg(not(feature = "use_semihosting"))]
 use panic_halt as _;
 #[cfg(feature = "use_semihosting")]
@@ -11,31 +13,68 @@ use bsp::ehal;
 use bsp::pac;
 use feather_m0 as bsp;
 
-use bsp::entry;
+use bsp::{entry,I2cSercom,Sda,Scl,I2c};
 
 use hal::delay::Delay;
+use hal::time::Hertz;
 use hal::clock::GenericClockController;
-use ehal::blocking::i2c;
 
-use pac::{CorePeripherals,Peripherals};
+use ehal::prelude::*;
 
-const LENGTH: usize = 1;
-const DHT_ADDRESS: u8 = 0x38;
+use pac::{CorePeripherals,Peripherals,PM};
+
 const TEMPERATURE_THRESHOLD: f32 = 25.0;
+const BUFFER_SIZE: u8 = 7;
 
-struct TemperatureSensorDriver<I2C> {
-    i2c: I2C
+struct DHT20 {
+    address: u8,
+    i2c: I2c,
+    temperature: f32,
+    humidity: f32
 }
 
-impl<I2C, E> TemperatureSensorDriver<I2C>
-where
-    I2C: i2c::WriteRead<Error = E>,
-{
-    pub fn read_temperature(&mut self) -> Result<u8, E> {
-        let mut temp = [0];
-        self.i2c
-            .write_read(DHT_ADDRESS, &[DHT_REG], &mut temp)
-            .and(Ok(temp[0]))
+impl DHT20 {
+    fn new(clocks: &mut GenericClockController, baud: impl Into<Hertz>, sercom: I2cSercom, pm: &mut PM, sda: impl Into<Sda>, scl: impl Into<Scl>, delay: &mut Delay) -> Self {
+        let mut buffer = [0];
+        let mut dht20 = Self {
+            address: 0x38,
+            i2c: bsp::i2c_master(clocks, baud, sercom, pm, sda, scl),
+            temperature: 0.0,
+            humidity: 0.0
+        };
+
+        delay.delay_ms(40u8); // delay 40ms for startup
+        dht20.i2c.write_read(dht20.address, &[0x71], &mut buffer).unwrap(); // get status word
+
+        if (buffer[0] & (1 << 3)) != 0 {
+            dht20.i2c.write(dht20.address, &[0x08, 0x00]).unwrap();
+            delay.delay_ms(10u8);
+        } // if status word is not 1 then send initialization and delay 10ms
+        
+        dht20
+    }
+
+    fn measure(&mut self, delay: &mut Delay) {
+        let mut measurement_data = [0u8; 7];
+
+        self.i2c.write_read(self.address, &[0xAC], &mut measurement_data).unwrap();
+
+        delay.delay_ms(80u8); // wait for measurement
+
+        
+        let mut measurement_completed = false;
+        while !measurement_completed {
+            delay.delay_ms(80u8);
+            self.i2c.write_read(self.address, &[0xAC], &mut measurement_data).unwrap();
+            measurement_completed = (measurement_data[0] & 0x80) == 0;
+        }
+
+        let humidity_raw = ((measurement_data[1] as u16) << 12) | ((measurement_data[2] as u16) << 4) | ((measurement_data[3] as u16) >> 4);
+        let temperature_raw = ((measurement_data[3] as u16 & 0x0F) << 16) | ((measurement_data[4] as u16) << 8) | measurement_data[5] as u16;
+
+        // Calculate humidity and temperature values
+        self.humidity =  ((humidity_raw as f32) * 100.0 / 1048576.0) as f32;
+        self.temperature = (((temperature_raw as f32) * 200.0 / 1048576.0) - 50.0) as f32;
     }
 }
 
@@ -45,22 +84,32 @@ fn main() -> ! {
     let core_peripherals = CorePeripherals::take().unwrap();
     let mut peripherals = Peripherals::take().unwrap();
 
-    let mut pins = bsp::Pins::new(peripherals.PORT);
+    let pins = bsp::Pins::new(peripherals.PORT);
 
-    // intialize clock controller
-    let mut clock_controller = GenericClockController::with_internal_32kosc(
+    let mut clocks = GenericClockController::with_external_32kosc(
         peripherals.GCLK,
         &mut peripherals.PM,
         &mut peripherals.SYSCTRL,
         &mut peripherals.NVMCTRL
-     );
+    );
 
     // initialize delay
-    let mut delay = Delay::new(core_peripherals.SYST, &mut clock_controller);
+    let mut delay = Delay::new(core_peripherals.SYST, &mut clocks);
 
-    // initialize I2C
-    let mut buffer: &mut [u8];
-    let mut dht_sen = i2c::WriteRead::write_read(i2c, DHT_ADDRESS, &[0], buffer);
+    // initialize DHT20
+    let mut dht20 = DHT20::new(&mut clocks, Hertz(100000), peripherals.SERCOM3, &mut peripherals.PM, pins.sda, pins.scl, &mut delay);
 
-    loop {}
+    // turn LED on if temperature is above TEMPERATURE_THRESHOLD
+    let mut red_led: bsp::RedLed = pin_alias!(pins.red_led).into();
+
+    loop {
+        delay.delay_ms(200u8);
+        dht20.measure(&mut delay);
+
+        if dht20.temperature >= TEMPERATURE_THRESHOLD {
+            red_led.set_high().unwrap();
+        } else {
+            red_led.set_low().unwrap();
+        }
+    }
 }
