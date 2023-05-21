55971f9cfbbe1f06b74233d0d8ca7f9f3e375946
HexExecute <hex.execute@gmail.com>
2023-05-21 16:32:18 -0400

    Created dht20.rs based on aht20 crate

---
+++ src/dht20.rs
@@ -0,0 +1,147 @@
+use bsp::ehal;
+use bsp::hal;
+use bsp::pac;
+use feather_m0 as bsp;
+
+use bitflags::bitflags;
+use ehal::blocking::i2c::WriteRead;
+use ehal::blocking::i2c::Write;
+use ehal::blocking::delay::DelayMs;
+
+
+const DHT20_ADDRESS: u8 = 0x38;
+
+bitflags! {
+    struct StatusFlags: u8 {
+        const BUSY = (1 << 7);
+        const MODE = ((1 << 6) | (1 << 5));
+        const CRC = (1 << 4);
+        const CALIBRATION_ENABLE = (1 << 3);
+        const FIFO_ENABLE = (1 << 2);
+        const FIFO_FULL = (1 << 1);
+        const FIFO_EMPTY = (1 << 0);
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum Error<E> {
+    Uncalibrated,
+    Bus(E),
+    Checksum
+}
+
+impl<E> core::convert::From<E> for Error<E> {
+    fn from(e: E) -> Self {
+        Error::Bus(e)
+    }
+}
+
+
+pub struct Humidity {
+    h: u32
+}
+
+impl Humidity {
+    fn rh(&self) -> f32 {
+        100.0 * (self.h as f32) / ((1 << 20) as f32)
+    }
+
+    pub fn raw(&self) -> u32 { self.h }
+}
+
+
+pub struct Temperature {
+    t: u32
+}
+
+impl Temperature {
+    pub fn celsius(&self) -> f32 {
+        (200.0 * (self.t as f32) / ((1 << 20) as f32)) - 50.0
+    }
+
+    pub fn raw(&self) -> u32 { self.t }
+}
+
+
+pub struct DHT20<I2C, D> {
+    i2c: I2C,
+    pub delay: D
+}
+
+
+impl<I2C, D, E> DHT20<I2C, D>
+where
+    I2C: WriteRead<Error = E> + Write<Error = E>,
+    D: DelayMs<u16>
+{
+    pub fn new(i2c: I2C, delay: D) -> Result<Self, Error<E>> {
+        let mut dev = Self { i2c, delay };
+
+        dev.reset()?;
+        dev.calibrate()?;
+
+        Ok(dev)
+    }
+
+    fn status(&mut self) -> Result<StatusFlags, E> {
+        let buf = &mut [0u8; 1];
+
+        self.i2c.write_read(DHT20_ADDRESS, &[0u8], buf)?;
+
+        Ok(StatusFlags::from_bits_truncate(buf[0]))
+    }
+
+    pub fn calibrate(&mut self) -> Result<(), Error<E>> {
+        self.i2c.write(DHT20_ADDRESS, &[0xE1, 0x08, 0x00])?;
+
+        while self.status()?.contains(StatusFlags::BUSY) {
+            self.delay.delay_ms(10);
+        }
+
+        while self.status()?.contains(StatusFlags::CALIBRATION_ENABLE) {
+            return Err(Error::Uncalibrated);
+        }
+
+        Ok(())
+    }
+
+    pub fn reset(&mut self) -> Result<(), E> {
+        self.i2c.write(DHT20_ADDRESS, &[0xBA])?;
+
+        self.delay.delay_ms(20);
+
+        Ok(())
+    }
+
+    pub fn read(&mut self) -> Result<(Humidity, Temperature), Error<E>> {
+        // lazy_static! {
+        //     static ref CRC: CrcAlgo<u8> = CrcAlgo::<u8>::new(49, 8, 0xFF, 0x00, false);
+        // }
+
+        self.i2c.write(DHT20_ADDRESS, &[0xAC, 0x33, 0x00])?;
+
+        while self.status()?.contains(StatusFlags::BUSY) {
+            self.delay.delay_ms(10);
+        }
+
+        let buf = &mut [0u8; 7];
+        self.i2c.write_read(DHT20_ADDRESS, &[0u8], buf)?;
+
+        // let crc = &mut 0u8;
+        // CRC.init_crc(crc);
+
+        // if CRC.update_crc(crc, &buf[..=5]) != buf[6] {
+        //     return Err(Error::Checksum);
+        // };
+
+        let status = StatusFlags::from_bits_truncate(buf[0]);
+        if !status.contains(StatusFlags::CALIBRATION_ENABLE) {
+            return Err(Error::Uncalibrated);
+        }
+
+        let hum = ((buf[1] as u32) << 12) | ((buf[2] as u32) << 4) | ((buf[3] as u32) >> 4);
+        let temp = (((buf[3] as u32) & 0x0f) << 16) | ((buf[4] as u32) << 8) | (buf[5] as u32);
+
+        Ok((Humidity { h: hum }, Temperature { t: temp }))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![no_std]
 #![no_main]
 
+use dht20::DHT20;
 #[cfg(not(feature = "use_semihosting"))]
 use panic_halt as _;
 #[cfg(feature = "use_semihosting")]
 use panic_semihosting as _;
+
+mod dht20;
 
 use bsp::ehal;
 use bsp::hal;
@@ -21,126 +24,143 @@ use hal::time::Hertz;
 use ehal::digital::v2::OutputPin;
 use ehal::prelude::*;
 
+
 use pac::{CorePeripherals, Peripherals, PM};
 
 const TEMPERATURE_THRESHOLD: f32 = 15.0;
 
-struct DHT20 {
-    i2c: I2c,
-    temperature: f32,
-    humidity: f32,
-}
 
-impl DHT20 {
-    fn new(
-        clocks: &mut GenericClockController,
-        baud: impl Into<Hertz>,
-        sercom: I2cSercom,
-        pm: &mut PM,
-        sda: impl Into<Sda>,
-        scl: impl Into<Scl>,
-        delay: &mut Delay,
-        led: &mut bsp::RedLed,
-    ) -> Self {
-        let mut buffer = [0u8];
-
-        // led.set_high().unwrap();
-
-        let mut dht20 = Self {
-            // i2c: {
-            //     let mut i2c = hal::sercom::i2c::Config::new(
-            //         &pm,
-            //         sercom,
-            //         hal::sercom::i2c::Pads::<Sercom3, Sda, Scl>::new(sda.into(), scl.into()),
-            //         10.mhz(),
-            //     );
-            //     i2c.set_baud(baud);
-            //     i2c.enable()
-            // },
-            i2c: i2c_master(clocks, baud, sercom, pm, sda, scl),
-            temperature: 0.0,
-            humidity: 0.0,
-        };
-
-        delay.delay_ms(40u8); // delay 40ms for start
-
-        // led.set_high().unwrap();
-
-        dht20.i2c
-            .read(0x71, &mut buffer)
-            .unwrap();
-
-        if buffer[0] & (1 << 3) != 0 {
-            dht20
-                .i2c
-                .write(0xbe, &[0x08, 0x00])
-                .unwrap();
-            delay.delay_ms(10u8);
-        }
-
-        led.set_high().unwrap();
-
-        dht20
-    }
-
-    fn measure(&mut self, delay: &mut Delay, led: &mut bsp::RedLed) {
-        let mut measurement_data = [0u8; 7];
-
-        self.i2c
-            .write(0x70, &[0xAC, 0x33, 0x00])
-            .unwrap();
-
-        delay.delay_ms(80u8); // wait for measurement
-
-        let mut status = [0u8];
-
-        self.i2c
-            .read(0x71, &mut status)
-            .unwrap();
-
-        led.set_high().unwrap();
-
-        let mut measurement_completed = false;
-        while !measurement_completed {
-            // check if Bit [7] of status value is 1
-            if status[0] & (1 << 7) == 0 { measurement_completed = true; }
-
-            delay.delay_ms(80u8); // wait for measurement
-
-            self.i2c
-                .read(0x71, &mut status)
-                .unwrap();
-        }
-
-        self.i2c
-            .read(0x71, &mut measurement_data)
-            .unwrap();
-
-
-        let raw_humidity = u32::from_be_bytes([0, measurement_data[0], measurement_data[1], measurement_data[2]]);
-        let raw_temperature = i32::from_be_bytes([measurement_data[3], measurement_data[4], 0, 0]);
-        let _crc = measurement_data[5];
-
-        self.humidity = raw_humidity as f32 / 10.0;
-        self.temperature = raw_temperature as f32 / 10.0;
-
-        // let mut measurement_completed = false;
-        // while !measurement_completed {
-        //     delay.delay_ms(80u8);
-        //     self.i2c
-        //         .write_read(self.address, &[0xAC], &mut measurement_data)
-        //         .unwrap();
-        //     measurement_completed = (measurement_data[0] & 0x80) == 0;
-        // }
-        //
-        // let humidity_raw = u16::from_be_bytes([measurement_data[1], measurement_data[2]]);
-        // let temperature_raw = u16::from_be_bytes([measurement_data[3], measurement_data[4]]);
-        //
-        // // Calculate humidity and temperature values
-        // self.humidity = (humidity_raw as f32) / 10.0;
-        // self.temperature = (temperature_raw as f32) / 10.0 as f32;
-    }
-}
+// struct DHT20 {
+//     i2c: I2c,
+//     temperature: f32,
+//     humidity: f32,
+//     delay: Delay
+// }
+//
+// impl DHT20 {
+//     fn new(
+//         clocks: &mut GenericClockController,
+//         baud: impl Into<Hertz>,
+//         sercom: I2cSercom,
+//         pm: &mut PM,
+//         sda: impl Into<Sda>,
+//         scl: impl Into<Scl>,
+//         mut delay: Delay,
+//         led: &mut bsp::RedLed,
+//     ) -> Self {
+//         let mut status_word = [0u8];
+//
+//         let mut dht20 = Self {
+//             // i2c: {
+//             //     let mut i2c = hal::sercom::i2c::Config::new(
+//             //         &pm,
+//             //         sercom,
+//             //         hal::sercom::i2c::Pads::<Sercom3, Sda, Scl>::new(sda.into(), scl.into()),
+//             //         10.mhz(),
+//             //     );
+//             //     i2c.set_baud(baud);
+//             //     i2c.enable()
+//             // },
+//             i2c: i2c_master(clocks, baud, sercom, pm, sda, scl),
+//             temperature: 0.0,
+//             humidity: 0.0,
+//             delay
+//         };
+//
+//         delay.delay_ms(20u8);
+//
+//         dht20.calibrate();
+//
+//         dht20.i2c
+//             .write_read(0x71, &[0x71], &mut status_word)
+//             .unwrap();
+//
+//         led.set_high().unwrap();
+//
+//         // if buffer[0] & 0x18 != 0x18 {
+//         //     dht20
+//         //         .i2c
+//         //         .write(0xbe, &[0x08, 0x00])
+//         //         .unwrap();
+//         //     delay.delay_ms(10u8);
+//         // }
+//
+//
+//         dht20
+//     }
+//
+//     fn status(&mut self) -> Result<StatusFlags, _> {
+//         let buf = &mut [0u8; 1];
+//         self.i2c.write_read(DHT20_ADDRESS, &[0u8], buf).unwrap();
+//
+//         StatusFlags { bits: buf }
+//
+//     }
+//
+//     fn calibrate(&mut self) {
+//         self.delay.delay_ms(20u8);
+//
+//         self.i2c
+//             .write()
+//     }
+//
+//     fn measure(&mut self, delay: &mut Delay, led: &mut bsp::RedLed) {
+//         let mut measurement_data = [0u8; 6];
+//
+//         self.i2c
+//             .write(0x70, &[0xAC, 0x33, 0x00])
+//             .unwrap();
+//
+//         delay.delay_ms(80u8); // wait for measurement
+//
+//         let mut status = [0u8];
+//
+//         self.i2c
+//             .read(0x71, &mut status)
+//             .unwrap();
+//
+//
+//         let mut measurement_completed = false;
+//         while !measurement_completed {
+//             // check if Bit [7] of status value is 1
+//             if status[0] & (1 << 7) == 0 { measurement_completed = true; }
+//
+//             delay.delay_ms(80u8); // wait for measurement
+//
+//             self.i2c
+//                 .read(0x71, &mut status)
+//                 .unwrap();
+//         }
+//
+//         self.i2c
+//             .read(0x71, &mut measurement_data)
+//             .unwrap();
+//
+//
+//         let raw_humidity = u32::from_be_bytes([0, measurement_data[0], measurement_data[1], measurement_data[2]]);
+//         let raw_temperature = i32::from_be_bytes([measurement_data[3], measurement_data[4], 0, 0]);
+//         let _crc = measurement_data[5];
+//
+//         self.humidity = raw_humidity as f32 / 10.0;
+//         self.temperature = raw_temperature as f32 / 10.0;
+//
+//         // let mut measurement_completed = false;
+//         // while !measurement_completed {
+//         //     delay.delay_ms(80u8);
+//         //     self.i2c
+//         //         .write_read(self.address, &[0xAC], &mut measurement_data)
+//         //         .unwrap();
+//         //     measurement_completed = (measurement_data[0] & 0x80) == 0;
+//         // }
+//         //
+//         // let humidity_raw = u16::from_be_bytes([measurement_data[1], measurement_data[2]]);
+//         // let temperature_raw = u16::from_be_bytes([measurement_data[3], measurement_data[4]]);
+//         //
+//         // // Calculate humidity and temperature values
+//         // self.humidity = (humidity_raw as f32) / 10.0;
+//         // self.temperature = (temperature_raw as f32) / 10.0 as f32;
+//     }
+// }
 
 #[entry]
 fn main() -> ! {
@@ -148,7 +168,7 @@ fn main() -> ! {
     let core_peripherals = CorePeripherals::take().unwrap();
     let mut peripherals = Peripherals::take().unwrap();
 
-    let pins = bsp::Pins::new(peripherals.PORT);
+    let mut pins = bsp::Pins::new(peripherals.PORT);
 
     // let pads = i2c::Pads::<Sercom3>::new(pins.pa)
 
@@ -164,23 +184,56 @@ fn main() -> ! {
 
     let mut red_led: bsp::RedLed = pin_alias!(pins.red_led).into();
 
-    // initialize DHT20
-
-    let mut dht20 = DHT20::new(
-        &mut clocks,
-        100.khz(),
-        peripherals.SERCOM3,
-        &mut peripherals.PM,
-        pins.sda,
-        pins.scl,
-        &mut delay,
-        &mut red_led,
+    let dht20 = DHT20::new(
+        i2c_master(
+            &mut clocks,
+            10.khz(),
+            peripherals.SERCOM3,
+            &mut peripherals.PM,
+            pins.sda,
+            pins.scl
+        ),
+        delay
     );
 
-    dht20.measure(&mut delay, &mut red_led);
+    match dht20 {
+        Err(_) => {
+            red_led.set_high().unwrap();
+        },
+        Ok(_) => {
+        }
+    }
+
+    // let dht20 = Aht20::new(
+    //     i2c_master(
+    //         &mut clocks,
+    //         100.khz(),
+    //         peripherals.SERCOM3,
+    //         &mut peripherals.PM,
+    //         pins.sda.into(),
+    //         pins.scl.into()
+    //     ),
+    //     &mut delay
+    // ).unwrap();
+
+    // dht20.calibrate();
+
+    // initialize DHT20
+    // let mut dht20 = DHT20::new(
+    //     &mut clocks,
+    //     100.khz(),
+    //     peripherals.SERCOM3,
+    //     &mut peripherals.PM,
+    //     pins.sda,
+    //     pins.scl,
+    //     delay,
+    //     &mut red_led,
+    // );
+    //
+    // dht20.measure(&mut delay, &mut red_led);
 
     // if dht20.temperature <= 0.0 {
-        red_led.set_high().unwrap();
+        // red_led.set_high().unwrap();
     // }
 
     // turn LED on if temperature is above TEMPERATURE_THRESHOLD
