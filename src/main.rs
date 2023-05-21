3a124a2a52e51d2e7fb48b2a3ca3b743e7f792d6
HexExecute <hex.execute@gmail.com>
2023-05-21 00:25:43 -0400

    Progress on measure

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 #![no_std]
 #![no_main]
 
-use bsp::ehal::blocking::i2c;
-use bsp::hal::sercom::i2c::Pads;
-use bsp::hal::sercom::Sercom3;
-use bsp::i2c_master;
-use bsp::pin_alias;
 #[cfg(not(feature = "use_semihosting"))]
 use panic_halt as _;
 #[cfg(feature = "use_semihosting")]
@@ -17,7 +12,7 @@ use bsp::pac;
 use feather_m0 as bsp;
 use hal::time::U32Ext;
 
-use bsp::{entry, I2c, I2cSercom, Scl, Sda};
+use bsp::{entry, I2c, I2cSercom, Scl, Sda, i2c_master, pin_alias};
 
 use hal::clock::GenericClockController;
 use hal::delay::Delay;
@@ -29,10 +24,8 @@ use ehal::prelude::*;
 use pac::{CorePeripherals, Peripherals, PM};
 
 const TEMPERATURE_THRESHOLD: f32 = 15.0;
-const BUFFER_SIZE: u8 = 7;
 
 struct DHT20 {
-    address: u8,
     i2c: I2c,
     temperature: f32,
     humidity: f32,
@@ -54,7 +47,6 @@ impl DHT20 {
         // led.set_high().unwrap();
 
         let mut dht20 = Self {
-            address: 0x38,
             // i2c: {
             //     let mut i2c = hal::sercom::i2c::Config::new(
             //         &pm,
@@ -78,7 +70,7 @@ impl DHT20 {
             .read(0x71, &mut buffer)
             .unwrap();
 
-        if buffer[0] != 1 {
+        if buffer[0] & (1 << 3) != 0 {
             dht20
                 .i2c
                 .write(0xbe, &[0x08, 0x00])
@@ -91,34 +83,43 @@ impl DHT20 {
         dht20
     }
 
-    fn measure(&mut self, delay: &mut Delay) {
+    fn measure(&mut self, delay: &mut Delay, led: &mut bsp::RedLed) {
         let mut measurement_data = [0u8; 7];
 
         self.i2c
-            .write(self.address, &[0xAC, 0x33, 0x00])
+            .write(0x70, &[0xAC, 0x33, 0x00])
             .unwrap();
 
         delay.delay_ms(80u8); // wait for measurement
 
+        let mut status = [0u8];
+
         self.i2c
-            .read(self.address, &mut measurement_data)
+            .read(0x71, &mut status)
             .unwrap();
+
+        led.set_high().unwrap();
 
         let mut measurement_completed = false;
         while !measurement_completed {
             // check if Bit [7] of status value is 1
-            if measurement_data[0] & (1 << 7) == 0 { measurement_completed = true; }
+            if status[0] & (1 << 7) == 0 { measurement_completed = true; }
 
             delay.delay_ms(80u8); // wait for measurement
 
             self.i2c
-                .read(self.address, &mut measurement_data)
+                .read(0x71, &mut status)
                 .unwrap();
         }
 
-        let raw_humidity = u32::from_be_bytes([0, measurement_data[1], measurement_data[2], measurement_data[3]]);
-        let raw_temperature = i32::from_be_bytes([measurement_data[4], measurement_data[5], 0, 0]);
-        let _crc = measurement_data[6];
+        self.i2c
+            .read(0x71, &mut measurement_data)
+            .unwrap();
+
+
+        let raw_humidity = u32::from_be_bytes([0, measurement_data[0], measurement_data[1], measurement_data[2]]);
+        let raw_temperature = i32::from_be_bytes([measurement_data[3], measurement_data[4], 0, 0]);
+        let _crc = measurement_data[5];
 
         self.humidity = raw_humidity as f32 / 10.0;
         self.temperature = raw_temperature as f32 / 10.0;
@@ -176,11 +177,11 @@ fn main() -> ! {
         &mut red_led,
     );
 
-    dht20.measure(&mut delay);
+    dht20.measure(&mut delay, &mut red_led);
 
-    if dht20.temperature > 0.0 {
+    // if dht20.temperature <= 0.0 {
         red_led.set_high().unwrap();
-    }
+    // }
 
     // turn LED on if temperature is above TEMPERATURE_THRESHOLD
 
